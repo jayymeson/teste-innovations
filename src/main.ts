062da0df5da7f78d1f35e82471ac9b5c254e5989
Jaymeson Mendes <jaymesonmendes@gmail.com>
2023-01-16 19:13:48 -0300

    feature: finish

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common/pipes';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('')
+    .setDescription(
+      'Aplicação de um teste para a Innovations Servicos Digitais',
+    )
+    .addTag('orders')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
